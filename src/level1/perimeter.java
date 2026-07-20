8bf244a19c462ba0e0e50e5319628312db4b10c0
jananialagappan <jananialagappan24@gmail.com>
2026-07-20 15:29:07 +0530

    p9

---
+++ src/level1/perimeter.java
@@ -0,0 +1,13 @@
+package level1;
+
+import java.util.Scanner;
+
+public class perimeter {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int l=sc.nextInt();
+        int b=sc.nextInt();
+        int peri=2*(l+b);
+        System.out.println(peri);
+    }
+}
