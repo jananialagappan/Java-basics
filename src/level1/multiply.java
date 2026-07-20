4f6ff8682755082ed497e4deb502a72c1f445465
jananialagappan <jananialagappan24@gmail.com>
2026-07-20 12:09:20 +0530

    p4

---
+++ src/level1/multiply.java
@@ -0,0 +1,13 @@
+package level1;
+
+import java.util.Scanner;
+
+public class multiply {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2= sc.nextInt();
+        int ans=n1*n2;
+        System.out.println("Multiplication of two numbers: "+ans);
+    }
+}
---
+++ src/level1/name.java
@@ -0,0 +1,10 @@
+package level1;
+
+public class name {
+    public static void main(String[] args) {
+        for (int i=1;i<=10;i++)
+        {
+            System.out.println("Janani");
+        }
+    }
+}
