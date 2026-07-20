96275fb8e1d8363c5998f4d0566cf2c3e230629d
jananialagappan <jananialagappan24@gmail.com>
2026-07-20 12:14:57 +0530

    Added Level 1 Java programs

---
+++ src/level1/addition.java
@@ -0,0 +1,15 @@
+package level1;
+
+import java.util.Scanner;
+
+public class addition {
+    public static void main(String[] args) {
+
+       System.out.println(add(10,15));
+    }
+    static int add(int a,int b)
+    {
+        int sum=a+b;
+        return sum;
+    }
+}
---
+++ src/level1/subtraction.java
@@ -0,0 +1,14 @@
+package level1;
+
+import java.util.Scanner;
+
+public class subtraction {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n1= sc.nextInt();
+        int n2= sc.nextInt();
+        int ans=n1-n2;
+        System.out.println("Subtraction of two numbers: "+ans);
+
+    }
+}
