15132d007d11d93a4d2930e6191fd566b9c458b3
jananialagappan <jananialagappan24@gmail.com>
2026-07-22 09:50:22 +0530

    level2

---
+++ src/level2/ebbill.java
@@ -0,0 +1,29 @@
+package level2;
+
+import java.util.Scanner;
+
+public class ebbill {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int units = sc.nextInt();
+        double bill;
+
+        if (units <= 100) {
+            bill = units * 1.5;
+        } else if (units <= 200) {
+            bill = (100 * 1.5) + ((units - 100) * 2.5);
+        } else {
+            bill = (100 * 1.5) + (100 * 2.5) + ((units - 200) * 4);
+        }
+
+        System.out.println(bill);
+    }
+}
+/*
+Slabs
+Units	Rate
+0–100	₹1.5/unit
+101–200	₹2.5/unit
+Above 200	₹4/unit
+ */
